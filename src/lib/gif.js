f57565837e6a2c1fb7b0161fbe307d8d8e23927a
Luke Karrys <lukekarrys@gmail.com>
2018-04-13 16:13:13 -0700

    no worker name

--- src/lib/gif.js
+++ src/lib/gif.js
@@ -1,7 +1,6 @@
 import Gif from 'gif.js/dist/gif.js'
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
-import GifWorker from 'file-loader?name=gif.worker.js!gif.js/dist/gif.worker.js'
-// TODO might not need file loader name param
+import GifWorker from 'file-loader!gif.js/dist/gif.worker.js'
 
 export default (options = {}) =>
   new Gif({
