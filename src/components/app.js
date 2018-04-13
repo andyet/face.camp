bdd938527550f90555ef5d8d2fffe281238b8def
Luke Karrys <lukekarrys@gmail.com>
2018-04-12 17:12:45 -0700

    sharing to slack

--- src/components/app.js
+++ src/components/app.js
@@ -1,38 +1,66 @@
 import { h, Component } from 'preact'
+import dateformat from 'dateformat'
 import Capture from '../components/capture'
 import Channels from '../components/channels'
 import Message from '../components/message'
+import fetchForm from '../lib/fetch-formdata'
 
 export default class App extends Component {
   state = {
     image: null,
     channel: null,
-    message: ''
+    message: '',
+    error: null,
+    uploading: false,
+    success: null
+  }
+
+  static defaultProps = {
+    message: 'Its my mug on facecamp'
   }
 
   handlePost = () => {
-    const { access_token: token } = this.props
-    const { image, channel, message = "It's my mug on Facecamp!" } = this.state
+    const { access_token: token, message: defaultMessage } = this.props
+    const { image, channel, message } = this.state
 
-    return console.log('this doesnt work yet', this.state)
+    this.setState({ uploading: true, success: null, error: null })
 
-    fetch('https://slack.com/api/chat.postMessage', {
-      data: JSON.stringify({
-        as_user: true,
-        channel: channel.id,
-        text: message
-      }),
+    fetchForm('https://slack.com/api/files.upload', {
       method: 'POST',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      })
+      data: {
+        token,
+        title: `facecamp upload ${dateformat(
+          new Date(),
+          'yyyy-mm-dd HH:MM:ss'
+        )}`,
+        channels: channel.id,
+        filetype: 'gif',
+        filename: 'facecamp.gif',
+        file: image
+      }
     })
+      .then((upload) =>
+        fetchForm('https://slack.com/api/chat.postMessage', {
+          method: 'POST',
+          data: {
+            token,
+            as_user: true,
+            channel: channel.id,
+            text: `*${message || defaultMessage}*\n${upload.file.url_private}`
+          }
+        })
+      )
+      .then((success) => {
+        this.setState({ uploading: false, success, error: null })
+      })
+      .catch((error) =>
+        this.setState({ uploading: false, success: null, error })
+      )
   }
 
   render(
     { access_token: token, team_name: team, logout },
-    { image, channel, message }
+    { image, channel, uploading, success, error }
   ) {
     return (
       <div>
@@ -45,6 +73,9 @@ export default class App extends Component {
         <Capture onChange={(image) => this.setState({ image })} />
         <Message onChange={(message) => this.setState({ message })} />
         {image && channel && <button onClick={this.handlePost}>Post</button>}
+        {uploading && <div>Uploading...</div>}
+        {success && <div>Success!</div>}
+        {error && <div>{error}</div>}
       </div>
     )
   }
