e7e7295a1ff4cb23ea2b31b865fd9d9e88d2e653
Luke Karrys <lukekarrys@gmail.com>
2018-04-19 15:25:36 -0700

    reduce build size

--- preact.config.js
+++ preact.config.js
@@ -7,6 +7,27 @@ export default (config, env, helpers) => {
     '/template.html'
   )
 
+  const babelLoader = helpers.getLoadersByName(config, 'babel-loader')[0].rule
+  babelLoader.options.plugins = babelLoader.options.plugins
+    .map((plugin) => {
+      const [name, options] = typeof plugin === 'string' ? [plugin, {}] : plugin
+
+      if (name.includes('transform-object-assign/')) {
+        return null
+      }
+
+      if (
+        name.includes('transform-object-rest-spread/') ||
+        name.includes('transform-react-jsx/')
+      ) {
+        options.useBuiltIns = true
+        return [name, options]
+      }
+
+      return plugin
+    })
+    .filter(Boolean)
+
   // Where we're going we don't need polyfills
   delete config.entry.polyfills
 }
