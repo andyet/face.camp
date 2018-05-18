f3fe44e39c0568abb38d37f5ebc513076fe48e6d
Luke Karrys <lukekarrys@gmail.com>
2018-05-17 17:54:24 -0700

    Update to beta of preact cli and refactor config

---
+++ scripts/config-helpers.js
@@ -0,0 +1,131 @@
+import { merge, omit } from 'lodash'
+
+const moreHelpers = {
+  getMinimizersByName(config, name) {
+    return (config.optimization.minimizer || [])
+      .filter((minimizer) => {
+        return minimizer.constructor.name === name
+      })
+      .map((minimizer, index) => ({ minimizer, index }))
+  },
+  setBabelOptions(config, type, getOptions) {
+    const names = Object.keys(getOptions)
+    const fullKeys = names.map((name) => `babel-${type.slice(0, -1)}-${name}`)
+
+    this.getLoadersByName('babel-loader').forEach(
+      (babel) =>
+        (babel.rule.options[type] = babel.rule.options[type]
+          .map((p) => {
+            const [name, options] = typeof p === 'string' ? [p, {}] : p
+            const nameIndex = fullKeys.findIndex((key) => name.includes(key))
+
+            if (nameIndex > -1) {
+              const newOptions = getOptions[names[nameIndex]]
+              return newOptions === null
+                ? null
+                : [name, merge(options, newOptions)]
+            }
+
+            return p
+          })
+          .filter(Boolean))
+    )
+  },
+  removeRule(config, rule) {
+    rule && config.module.rules.splice(rule.index, 1)
+  },
+  removePlugin(config, plugin) {
+    plugin && config.plugins.splice(plugin.index, 1)
+  },
+  removeEntry(config, entry) {
+    delete config.entry[entry]
+  },
+  removeMinimizers(config) {
+    config.optimization.minimizer = []
+  },
+  removeServiceWorker(config) {
+    this.getPluginsByName('SWPrecacheWebpackPlugin').forEach((p) =>
+      this.removePlugin(p)
+    )
+    this.getPluginsByName('DefinePlugin').forEach(({ plugin }) => {
+      if (plugin.definitions['process.env.ADD_SW']) {
+        plugin.definitions['process.env.ADD_SW'] = false
+      }
+    })
+  },
+  removeAsyncRoutes(config) {
+    const jsRules = this.getRulesByMatchingFile('src/routes/home.js')
+    jsRules.forEach(({ rule, index }) => {
+      if (
+        typeof rule.loader === 'string' &&
+        rule.loader.endsWith('async-component-loader')
+      ) {
+        this.removeRule({ index })
+      }
+    })
+  },
+  setUglifyOptions(config, options) {
+    this.getMinimizersByName('UglifyJsPlugin').forEach(({ minimizer }) => {
+      merge(minimizer.options, options)
+    })
+  },
+  setPostCssOptions(config, options) {
+    this.getLoadersByName('postcss-loader').forEach(({ loader }) =>
+      merge(loader.options, options)
+    )
+  },
+  setHtmlOptions(config, options) {
+    this.getPluginsByName('HtmlWebpackPlugin').forEach((html) =>
+      merge(html.plugin.options, options)
+    )
+  },
+  debug(config) {
+    const plugins = this.getPlugins()
+    const htmls = this.getPluginsByName('HtmlWebpackPlugin')
+    const babels = this.getLoadersByName('babel-loader')
+    const uglifys = this.getMinimizersByName('UglifyJsPlugin')
+    const postcss = this.getLoadersByName('postcss-loader')
+    const js = this.getRulesByMatchingFile('file.js')
+
+    const debug = {
+      plugins: plugins.map(({ plugin }) => plugin.constructor.name),
+      html: htmls.map(({ plugin }) => omit(plugin.options, 'config')),
+      postcss: postcss.map(({ loader }) => loader.options),
+      js: js.map(({ rule }) => rule.loader),
+      'babel-presets': babels.map(({ rule }) => rule.options.presets),
+      'babel-plugins': babels.map(({ rule }) => rule.options.plugins),
+      min: uglifys.map(({ minimizer }) => minimizer.options)
+    }
+
+    process.stdout.write(JSON.stringify(debug, null, 2) + '\n')
+    throw new Error('Stopping to debug config')
+  }
+}
+
+export default (config, helpers) => {
+  const res = {}
+
+  // Bind all methods from helpers to themselves with the config as the
+  // first argument
+  const proto = Object.getPrototypeOf(helpers)
+  for (let name of Object.getOwnPropertyNames(proto)) {
+    const method = helpers[name]
+    const descriptor = Object.getOwnPropertyDescriptor(proto, name)
+
+    if (descriptor.value instanceof Function && name !== 'constructor') {
+      res[name] = method.bind(helpers, config)
+    }
+  }
+
+  // Also bind all methods in moreHelpers the same way so that they can call
+  // the other helpers by using `this`
+  Object.keys(moreHelpers).forEach((name) => {
+    const method = moreHelpers[name]
+    res[name] = method.bind(res, config)
+  })
+
+  return res
+}
+
+export const env = (k, def) =>
+  k in process.env ? process.env[k] !== 'false' : def
