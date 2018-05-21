ada4238b2b7da2473f9e2be38bfb8d828736dd93
Luke Karrys <lukekarrys@gmail.com>
2018-05-21 14:14:38 -0700

    privacy

--- src/routes/privacy.js
+++ src/routes/privacy.js
@@ -4,11 +4,15 @@ import Splash from '../components/splash'
 export default () => (
   <Splash>
     <p>
-      &yet will never to gather, store, or sell information about you, log your
-      messages or gifs, or engage in any other behavior that would compromise
-      your privacy and security in any way. Facecamp is a static web application
-      with no database, so your gifs and messages are uploaded directly to Slack
-      with no data passing through our servers.
+      &yet will never to gather, store, or sell information about you or your
+      team, log your messages or gifs, or engage in any other behavior that
+      would compromise your privacy and security in any way. Facecamp requests
+      access to your team’s public and private channels, direct messages, and
+      user information only to present a list of conversations to decide where
+      to upload your gifs, and this information is only requested through a
+      browser and used to communicate directly with Slack's API. Facecamp is a
+      static web application with no database, so your gifs and messages are
+      uploaded directly to Slack with no data passing through our servers.
     </p>
   </Splash>
 )
