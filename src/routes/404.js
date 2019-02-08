87094975b39adae1300eabf1e7f2666871026cda
Luke Karrys <lukekarrys@gmail.com>
2019-02-08 12:26:19 -0700

    add 404 page; closes #65

---
+++ src/routes/404.js
@@ -0,0 +1,10 @@
+import { h } from 'preact'
+import Splash from '../components/splash'
+
+export default () => (
+  <Splash>
+    <p>
+      404, y'all. Maybe go <a href="/">home</a>.
+    </p>
+  </Splash>
+)
