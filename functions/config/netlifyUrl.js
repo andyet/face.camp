617b79829c4b8c579c716575047f8a22b31eefdb
Luke Karrys <lukekarrys@gmail.com>
2020-04-23 15:08:58 -0700

    comment

--- functions/config/netlifyUrl.js
+++ functions/config/netlifyUrl.js
@@ -1,3 +1,4 @@
+// Copied from https://github.com/depadiernos/deploy-id-test/blob/22dd145ac2d911fb7cba93e3a7edd8436f39a695/functions/getdeploy/getdeploy.js#L3
 module.exports = (context) => {
   const data = context.clientContext.custom.netlify
   const parsedData = JSON.parse(Buffer.from(data, 'base64').toString('utf-8'))
